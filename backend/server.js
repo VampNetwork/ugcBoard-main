ee95396f6b499117a66b165f438a78fc3d660670
Bijan Asadi <bijan@MacBookAir-434.lan>
2025-05-08 09:35:01 -0700

    Add root route to backend for better user experience

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,12 @@ const fileRoutes = require('./routes/file.routes');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(config.mongoUri, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
